eafbb0b51b0010ebbd80ae4fc415cff3c2884738
oyebode23 <38587565+oyebode23@users.noreply.github.com>
2019-10-29 03:10:17 -0400

    adding ec2

---
+++ ec2.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+#access_key = "${var.aws_access_key}"
+#secret_key = "${var.aws_secret_key}"
+region = "${var.aws_region}"
+}
+
+# Terraform state will be stored in S3
+terraform {
+backend "s3" {
+bucket = "forms3"
+key    = "terraform.tfstate"
+region = "us-east-1"
+ }
+}
+
+
+resource "aws_instance" "testing" {
+ami = "${var.ami}"
+vpc_security_group_ids = ["${aws_security_group.TFSG.id}"]
+subnet_id = "${aws_subnet.TFSUB.id}"
+instance_type = "t2.micro"
+key_name = "TEST"
+tags = {
+  Name = "${var.aws_instance_name}"
+ }
+}
+
