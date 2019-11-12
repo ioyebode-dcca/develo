6cb7c7f5a99481f6126561821e588a1522ff736c
oyebode23 <38587565+oyebode23@users.noreply.github.com>
2019-11-11 22:37:51 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,13 +5,13 @@ region = "${var.aws_region}"
 }
 
 # Terraform state will be stored in S3
-terraform {
-backend "s3" {
-bucket = "forms3"
-key    = "terraform.tfstate"
-region = "us-east-1"
- }
-}
+//terraform {
+//backend "s3" {
+//bucket = "forms3"
+//key    = "terraform.tfstate"
+//region = "us-east-1"
+// }
+//}
 
 
 resource "aws_instance" "testing" {
